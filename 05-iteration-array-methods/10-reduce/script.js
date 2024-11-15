88161509d516782529fa9665e970fd3e0931783b
palmcivet7 <palmcivet@proton.me>
2024-11-15 12:46:57 +0000

    complete array challenges

---
+++ 05-iteration-array-methods/10-reduce/script.js
@@ -0,0 +1,47 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const sum = numbers.reduce(function (accumulator, currentValue) {
+  return accumulator + currentValue;
+}, 0); // starts at 0th index
+console.log(sum); // 55 // adds all of the numbers
+
+// alternative
+const sum2 = numbers.reduce((acc, cur) => acc + cur, 0);
+console.log(sum2);
+
+// Using a for loop
+const sum3 = () => {
+  let acc = 0;
+  for (const cur of numbers) {
+    acc = acc + cur;
+  }
+  return acc;
+};
+console.log(sum3());
+
+//
+const cart = [
+  {
+    id: 1,
+    name: 'Product 1',
+    price: 130,
+  },
+  {
+    id: 2,
+    name: 'Product 2',
+    price: 150,
+  },
+  {
+    id: 3,
+    name: 'Product 3',
+    price: 175,
+  },
+];
+
+const totalPrice = cart.reduce((acc, cur) => acc + cur.price, 0);
+console.log(totalPrice);
+
+const total = cart.reduce(function (acc, product) {
+  return acc + product.price;
+}, 0);
+console.log(total);
